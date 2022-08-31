601b5364fe1b46fa328a38195b2b6f7fb33144d1
medicalanthrax <109490810+medicalanthrax@users.noreply.github.com>
2022-08-31 17:11:56 -0500

    Dice Roller

---
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        val resultTextView : TextView = findViewById(R.id.textView)
+
+        val dice = Dice(54)
+
+        rollButton.setOnClickListener{
+            resultTextView.text = dice.roll().toString()
+        }
+    }
+}
+
+class Dice(private val numSides:Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
+
