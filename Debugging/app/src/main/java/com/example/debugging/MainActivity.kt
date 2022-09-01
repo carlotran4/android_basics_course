a7f188e1b9d2a65488cf208da4a56026773de229
medicalanthrax <109490810+medicalanthrax@users.noreply.github.com>
2022-09-01 14:35:13 -0500

    debugging

--- Debugging/app/src/main/java/com/example/debugging/MainActivity.kt
+++ Debugging/app/src/main/java/com/example/debugging/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.debugging
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 
 private const val TAG = "MainActivity"
 
@@ -10,15 +11,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        logging()
+
+         val helloTextView:TextView = findViewById(R.id.division_textview)
+
         division()
+
     }
 
     fun division() {
         val numerator = 60
         var denominator = 4
-        repeat(5) {
-            Log.v(TAG, "${numerator / denominator}")
+        repeat(4) {
+            Thread.sleep(3000)
+            findViewById<TextView>(R.id.division_textview).setText("${numerator/denominator}")
             denominator--
         }
     }
