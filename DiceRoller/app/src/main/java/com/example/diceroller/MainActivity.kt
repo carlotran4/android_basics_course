7d3d722c5c53721b00b4b5108f02040e562bee5b
medicalanthrax <109490810+medicalanthrax@users.noreply.github.com>
2022-09-01 11:09:57 -0500

    dice roller

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -11,12 +11,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
-        val resultTextView : TextView = findViewById(R.id.textView)
+        val diceImage : ImageView = findViewById(R.id.imageView)
 
-        val dice = Dice(54)
+        val dice = Dice(6)
+
+        fun rolldice() {
+            val foo = dice.roll()
+            val drawableResource = when(foo){
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                6 -> R.drawable.dice_6
+                else -> R.drawable.dice_1
+            }
+            diceImage.setImageResource(drawableResource)
+            diceImage.contentDescription = foo.toString()
+        }
+        rolldice()
 
         rollButton.setOnClickListener{
-            resultTextView.text = dice.roll().toString()
+            rolldice()
         }
     }
 }
@@ -26,4 +42,6 @@ class Dice(private val numSides:Int) {
         return (1..numSides).random()
     }
 }
+
+
 
